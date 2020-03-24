269f2df8c31f39e89bdccc32d4a79fb7ef2dc0cb
fernando Prado <falconp._007@hotmail.com>
2020-03-24 08:05:31 -0500

    initial commit

---
+++ src/test/java/com/co/kerbero/eureka/ServiceEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.kerbero.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
